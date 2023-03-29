7f7cec02c19c8bb076579afda066883070c85221
devsalman247 <salman247.dev@gmail.com>
2023-03-29 13:41:44 +0500

    added new section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { Header } from './components/Header/app.component';
+import { About } from './components/About/app.component';
 
 @NgModule({
-  declarations: [AppComponent, Header],
+  declarations: [AppComponent, Header, About],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/About/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './app.component.html',
+})
+export class About {
+  title = 'About';
+}
